bee742df1709acfda361f235be4a4d951b15fbb5
hbjang <13klove@naver.com>
2020-07-29 23:40:50 +0900

    first push

---
+++ src/main/java/com/op/gg/ogj/OgjApplication.java
@@ -0,0 +1,13 @@
+package com.op.gg.ogj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/op/gg/ogj/OgjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.op.gg.ogj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
