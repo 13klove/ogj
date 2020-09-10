2625b47c219338c96bf75647ac209b1693584024
hbjang <13klove@naver.com>
2020-09-10 21:35:42 +0900

    valid update2

--- src/main/java/com/op/gg/ogj/map/service/valid/MapValidService.java
+++ src/main/java/com/op/gg/ogj/map/service/valid/MapValidService.java
@@ -9,9 +9,11 @@ import com.op.gg.ogj.map.repository.MapJpaRepository;
 import com.op.gg.ogj.map.valid.MapValid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MapValidService {
 
     public final MapJpaRepository mapJpaRepository;
